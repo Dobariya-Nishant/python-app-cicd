29e2009b0588ef0e2b1790bbb7211e6e28d59a7e
Nishant <nishant.lakhanitechnologies@gmail.com>
2025-08-12 14:15:11 +0530

    vpc

---
+++ infra/deploy/vpc.tf
@@ -0,0 +1,21 @@
+##########################
+# Network infrastructure #
+##########################
+
+resource "aws_vpc" "this" {
+  cidr_block = "10.1.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+}
+
+##########################################################
+# Internate Gateway needed for inbound access to the ALB #
+##########################################################
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name= "${local.prefix}"
+  }
+}
